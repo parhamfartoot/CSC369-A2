146f1fccce3837109e7112870d5311b5b94bea92
parham <fartootp@gmail.com>
2020-10-19 20:06:08 -0400

    Initial commit

---
+++ jobs.c
@@ -0,0 +1,149 @@
+// ------------
+// This code is provided solely for the personal and private use of
+// students taking the CSC369H5 course at the University of Toronto.
+// Copying for purposes other than this use is expressly prohibited.
+// All forms of distribution of this code, whether as given or with
+// any changes, are expressly prohibited.
+//
+// Authors: Bogdan Simion
+//
+// All of the files in this directory and all subdirectories are:
+// Copyright (c) 2019 Bogdan Simion
+// -------------
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include "executor.h"
+
+extern struct executor tassadar;
+
+
+/**
+ * Populate the job lists by parsing a file where each line has
+ * the following structure:
+ *
+ * <id> <type> <num_resources> <resource_id_0> <resource_id_1> ...
+ *
+ * Each job is added to the queue that corresponds with its job type.
+ */
+void parse_jobs(char *file_name) {
+    int id;
+    struct job *cur_job;
+    struct admission_queue *cur_queue;
+    enum job_type jtype;
+    int num_resources, i;
+    FILE *f = fopen(file_name, "r");
+
+    /* parse file */
+    while (fscanf(f, "%d %d %d", &id, (int*) &jtype, (int*) &num_resources) == 3) {
+
+        /* construct job */
+        cur_job = malloc(sizeof(struct job));
+        cur_job->id = id;
+        cur_job->type = jtype;
+        cur_job->num_resources = num_resources;
+        cur_job->resources = malloc(num_resources * sizeof(int));
+
+        int resource_id; 
+				for(i = 0; i < num_resources; i++) {
+				    fscanf(f, "%d ", &resource_id);
+				    cur_job->resources[i] = resource_id;
+				    tassadar.resource_utilization_check[resource_id]++;
+				}
+				
+				assign_processor(cur_job);
+
+        /* append new job to head of corresponding list */
+        cur_queue = &tassadar.admission_queues[jtype];
+        cur_job->next = cur_queue->pending_jobs;
+        cur_queue->pending_jobs = cur_job;
+        cur_queue->pending_admission++;
+    }
+
+    fclose(f);
+}
+
+/*
+ * Magic algorithm to assign a processor to a job.
+ */
+void assign_processor(struct job* job) {
+    int i, proc = job->resources[0];
+    for(i = 1; i < job->num_resources; i++) {
+        if(proc < job->resources[i]) {
+            proc = job->resources[i];
+        }
+    }
+    job->processor = proc % NUM_PROCESSORS;
+}
+
+
+void do_stuff(struct job *job) {
+    /* Job prints its id, its type, and its assigned processor */
+    printf("%d %d %d\n", job->id, job->type, job->processor);
+}
+
+
+
+/**
+ * TODO: Fill in this function
+ *
+ * Do all of the work required to prepare the executor
+ * before any jobs start coming
+ * 
+ */
+void init_executor() {
+
+
+
+}
+
+
+/**
+ * TODO: Fill in this function
+ *
+ * Handles an admission queue passed in through the arg (see the executor.c file). 
+ * Bring jobs into this admission queue as room becomes available in it. 
+ * As new jobs are added to this admission queue (and are therefore ready to be taken
+ * for execution), the corresponding execute thread must become aware of this.
+ * 
+ */
+void *admit_jobs(void *arg) {
+    struct admission_queue *q = arg;
+
+    // This is just so that the code compiles without warnings
+    // Remove this line once you implement this function
+    q = q;
+
+    return NULL;
+}
+
+
+/**
+ * TODO: Fill in this function
+ *
+ * Moves jobs from a single admission queue of the executor. 
+ * Jobs must acquire the required resource locks before being able to execute. 
+ *
+ * Note: You do not need to spawn any new threads in here to simulate the processors.
+ * When a job acquires all its required resources, it will execute do_stuff.
+ * When do_stuff is finished, the job is considered to have completed.
+ *
+ * Once a job has completed, the admission thread must be notified since room
+ * just became available in the queue. Be careful to record the job's completion
+ * on its assigned processor and keep track of resources utilized. 
+ *
+ * Note: No printf statements are allowed in your final jobs.c code, 
+ * other than the one from do_stuff!
+ */
+void *execute_jobs(void *arg) {
+    struct admission_queue *q = arg;
+
+    // This is just so that the code compiles without warnings
+    // Remove this line once you implement this function
+    q = q;
+
+
+    return NULL;
+}
